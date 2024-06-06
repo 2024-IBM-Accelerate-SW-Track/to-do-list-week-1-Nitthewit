5b1e891c26f24feb16c35f395587a0510ac04b80
Nitthewit <nithin.nambi04@gmail.com>
2024-06-05 20:03:56 -0400

    commit 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavbarComp from './component/navigation/NavbarComp';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <NavbarComp/>
+      <TextField
+            label="Add New Item"
+            variant="outlined"
+            data-testid="new-item-textfield"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ marginLeft: "10px" }}
+            data-testid="new-item-button"
+          ></Button>
     </div>
   );
 }
